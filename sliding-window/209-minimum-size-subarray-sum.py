a68dcfde285799148fdb5ecc947378cf0cb1326d
Bhavesh Jagtap <bhaveshjagtap0723@gmail.com>
2026-06-11 18:03:19 +0530

    perf: optimize minimum length calculation

--- sliding-window/209-minimum-size-subarray-sum.py
+++ sliding-window/209-minimum-size-subarray-sum.py
@@ -66,8 +66,9 @@ class Solution:
             current_sum += nums[right]
             
             while current_sum >= target:
+                min_len = min(min_len, right - left + 1)
                 current_sum -= nums[left]
                 left += 1
         
-        return 0
+        return 0 if min_len == float('inf') else min_len
 
